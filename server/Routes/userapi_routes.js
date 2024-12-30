582208e0f83d7734b7fac560147894c03a685a4c
Ken Xu <kenxu1997@gmail.com>
2024-12-30 13:40:47 -0500

    setupDB and server

---
+++ server/Routes/userapi_routes.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+
+const express = require("express");
+var router = express.Router();
+
+const SHA224 = require("sha224");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+const CRYPTO = require("crypto");
+// const nodemailer = require("nodemailer");
+var nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
+const jwt = require("jsonwebtoken");
+const mongo = require("mongodb");
+const fs = require("fs");
+const path = require("path");
+
+var transport = nodemailer.createTransport(
+  smtpTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    secure: true,
+    port: 465,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_APP_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  })
+);
+
+// route test email style
+router.post("/testEmail/", jsonParser, async function (req, res, next) {
+  fs.readFile(
+    path.join(__dirname, "../Email/resetPassEmailTemplate.html"),
+    "utf8",
+    async (err, html) => {
+      if (err) {
+        return console.log(err);
+      }
+      var user = {};
+      await ClientModel.findOne({
+        _id: req.body._id,
+      })
+        .then((response) => {
+          user = response;
+        })
+        .catch((err) => {
+          console.log("err", err);
+          return res;
+        });
+      const mailData = {
+        from: '"YD Auto Group" process.env.EMAIL',
+        to: user.email,
+        subject: "Welcome to YD Auto Group VIP",
+        // text: `Welcome to ydVIP, ${req.body.first_name}!`,
+        html: html.replace("${recipientName}", user.first_name),
+        // .replace("${recipientEmail}", user.email)
+        // .replace("${recipientFirstName}", user.first_name)
+        // .replace("${recipientLastName}", user.last_name)
+        // .replace("${websiteUrl}", process.env.CLIENTURL)
+        attachments: [
+          {
+            filename: "ydlogoblack.png",
+            path: path.join(__dirname, "../Email/Assets/ydlogoblack.png"),
+            cid: "logo",
+          },
+        ],
+      };
+
+      transport.sendMail(mailData, function (err, info) {
+        if (err) console.log(err);
+        else console.log(info);
+      });
+
+      res.status(200).json({ message: "Email sent" });
+      return res;
+    }
+  );
+
+  //curl -X POST -H "Content-Type: application/json" -d '{"email": " ", "first_name": "John"}' http://localhost:3000/userExpress/testEmail/
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,139 @@
+const express = require("express"); //Line 1
+const app = express(); //Line 2
+const cors = require("cors");
+const path = require("path");
+const { ClientModel } = require("./Model/User.js");
+app.use(express.static(path.join(__dirname, "../build")));
+const jwt = require("jsonwebtoken");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoString = process.env.DATABASE_URL;
+app.use(express.json());
+
+const port = process.env.PORT || 3001; //Line 3
+
+mongoose.connect(mongoString, {
+  useNewUrlParser: true,
+});
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+const corsOptions = {
+  origin: true, //access-control-allow-origin
+  credentials: true, //access-control-allow-credentials:true
+  exposedHeaders: ["set-cookie"],
+  optionSuccessStatus: 200,
+};
+
+app.use(function (req, res, next) {
+  console.log("req", req.path);
+  next();
+});
+
+app.use(cors(corsOptions));
+
+// make new api for server health check
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.use(async function (req, res, next) {
+  var whitelist = [
+    "login",
+    "forgot",
+    "resetpassword",
+    "signup",
+    "logout",
+    "forgotusername",
+    "forgotpassword",
+    "token_validation_check",
+    "token",
+    "validateResetToken",
+  ];
+  var isInWhitelist = false;
+  for (var i = 0; i < whitelist.length; i++) {
+    if (req.path.includes(whitelist[i])) {
+      isInWhitelist = true;
+      break;
+    }
+  }
+  console.log("whitelist", req.get("host"));
+  console.log("whitelist", process.env.HOSTWHITELIST.split(","));
+  console.log("whitelist", req.get("host"));
+  if (
+    isInWhitelist ||
+    process.env.HOSTWHITELIST.split(",").includes(req.get("host"))
+  ) {
+    next();
+  } else {
+    if (req.headers.cookie) {
+      var user_data = JSON.parse(
+        decodeURIComponent(req.headers.cookie).split("USER-SESSION=j:")[1]
+      );
+      console.log("user_data", user_data);
+      const validate_user = async (user_data) => {
+        var auth_token = user_data["auth_token"];
+
+        //decode token
+        var decoded = jwt.verify(auth_token, process.env.JWT_SECRET);
+        console.log("decoded", decoded);
+        //check if user exists
+        var user = await ClientModel.findOne({
+          _id: decoded._id,
+        }).catch((err) => {
+          res.status(401).send("Access denied");
+        });
+
+        if (user) {
+          // check if user = user_data on  _id, firstname, lastname, email, usertype, password
+          if (
+            user._id == user_data._id &&
+            user.first_name == user_data.first_name &&
+            user.last_name == user_data.last_name &&
+            user.email == user_data.email &&
+            user.usertype == user_data.usertype &&
+            user.password == user_data.password
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      };
+      var valid = await validate_user(user_data).catch((err) => {
+        console.log(err);
+      });
+      if (valid) {
+        next();
+      } else {
+        //this means jwt expired
+        res.status(401).send("Access denied");
+      }
+    } else {
+      //block express apis
+      if (req.path.includes("Express")) {
+        res.status(401).send("Access denied");
+      } else {
+        //redirect user to login
+        res.status(401).redirect("/login/?next=" + req.path);
+        console.log("redirecting to login");
+      }
+    }
+  }
+});
+
+var user = require("./Routes/userapi_routes.js");
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.use("/", (req, res) => res.send("Hello World!"));
+app.use("/userExpress", user);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
